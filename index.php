c27675fbc3c301151a94c80d44b1b7d798330935
4q-u4 <se.danielmadi@gmail.com>
2023-12-02 19:47:58 +0200

    helpdesk

--- index.php
+++ index.php
@@ -1,1 +1,60 @@
-<h1>Hello</h1>
+<?php
+include 'functions.php';
+// Connect to MySQL using the below function
+$pdo = pdo_connect_mysql();
+// MySQL query that retrieves all the tickets from the database
+$stmt = $pdo->prepare('SELECT * FROM tickets ORDER BY created DESC');
+$stmt->execute();
+$tickets = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tickets</title>
+    <!-- Link to your CSS stylesheet -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<?=template_header('Tickets')?>
+
+<div class="content home">
+
+	<h2>Tickets</h2>
+
+	<p>Welcome to the index page. You can view the list of tickets below.</p>
+
+	<div class="btns">
+		<a href="create.php" class="btn">Create Ticket</a>
+	</div>
+
+	<div class="tickets-list">
+		<?php foreach ($tickets as $ticket): ?>
+		<a href="view.php?id=<?=$ticket['id']?>" class="ticket">
+			<span class="con">
+				<?php if ($ticket['status'] == 'open'): ?>
+				<i class="far fa-clock fa-2x"></i>
+				<?php elseif ($ticket['status'] == 'resolved'): ?>
+				<i class="fas fa-check fa-2x"></i>
+				<?php elseif ($ticket['status'] == 'closed'): ?>
+				<i class="fas fa-times fa-2x"></i>
+				<?php endif; ?>
+			</span>
+			<span class="con">
+				<span class="title"><?=htmlspecialchars($ticket['title'], ENT_QUOTES)?></span>
+				<span class="msg"><?=htmlspecialchars($ticket['msg'], ENT_QUOTES)?></span>
+			</span>
+			<span class="con created"><?=date('F dS, G:ia', strtotime($ticket['created']))?></span>
+		</a>
+		<?php endforeach; ?>
+	</div>
+
+</div>
+
+<?=template_footer()?>
+
+</body>
+</html>
